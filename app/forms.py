0bc010119b8a12cb789e7fc03fab4a807c1cbb03
mattstermh93 <mattstermh@hotmail.com>
2018-07-23 13:10:27 -0400

    css working, forms loaded, models page active

---
+++ app/forms.py
@@ -0,0 +1,45 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, EqualTo, Email, Length, ValidationError
+from app.models import User
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Submit')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[Length(min=6, max=24), DataRequired()])
+    first_name = StringField('First Name', validators=[Length(min=6, max=24), DataRequired()])
+    last_name = StringField('Last Name', validators=[Length(min=6, max=24), DataRequired()])
+    email = StringField('Email Address', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password', message='Passwords must match')])
+    submit = SubmitField('Submit')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Username already taken.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Email already taken.')
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email Address', validators=[DataRequired(), Email()])
+    submit = SubmitField('Submit')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Username already taken.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Email already taken.')
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,65 @@
 from app import app, db
 from flask import render_template, redirect, url_for, flash, request
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from flask_login import current_user, login_user, logout_user, login_required
+from app.models import User
+from werkzeug.urls import url_parse
 
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index')
 def index():
     return render_template('index.html', title='Home Page')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(username=login_form.username.data).first()
+        if user is None or not user.check_password(login_form.password.data):
+            flash('Invalid login credentials')
+            return redirect(url_for('login'))
+        login_user(user, remember=login_form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        flash('Thanks for logging in {}!'.format(current_user.username))
+        return redirect(next_page)
+    return render_template('login.html', form=login_form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    register_form = RegistrationForm()
+    if register_form.validate_on_submit():
+        user = User(username=register_form.username.data, email=register_form.email.data)
+        user.set_password(register_form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Thanks for registering! You can now view your profile.')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=register_form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/profile', methods=['GET', 'PUT'])
+def profile():
+    return render_template("profile.html", title='Profile')
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
