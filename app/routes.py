edd73a41e14b330f3278c93c02ebf96f35217d97
mattstermh93 <mattstermh@hotmail.com>
2018-07-23 11:33:17 -0400

    page loads, no more bugs

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,8 @@
+from app import app, db
+from flask import render_template, redirect, url_for, flash, request
+from flask_login import current_user, login_user, logout_user, login_required
+
+
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index')
 def index():
