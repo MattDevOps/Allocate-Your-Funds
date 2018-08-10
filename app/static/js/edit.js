7ba87dd2f06cc5c6940c26ae38d55bc1ff26cc71
mattstermh93 <mattstermh@hotmail.com>
2018-08-10 12:16:16 -0400

    added 'required' placeholders to edit profile

---
+++ app/static/js/edit.js
@@ -0,0 +1,4 @@
+$("#full_name").attr("Placeholder", "*Required");
+$("#risk").attr("Placeholder", "*Required");
+$("#salary").attr("Placeholder", "*Required - numbers only");
+$("#age").attr("Placeholder", "*Required");
